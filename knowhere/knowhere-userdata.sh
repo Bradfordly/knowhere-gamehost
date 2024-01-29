4558be192c0b71db872d923599b04a7fb4bbc2b6
Bradfordly <39799881+Bradfordly@users.noreply.github.com>
2024-01-29 16:22:11 +0000

    rename userdata file

--- knowhere/knowhere-userdata.sh
+++ knowhere/knowhere-userdata.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ### config ###
-S3_BUCKET_NAME=s3://bradfordly-things
-MINECRAFT_BINARY_URL=https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar
-MINECRAFT_SAVE_FILE=/games/saves/minecraft/ce_plays_mc.zip
-MINECRAFT_DIRECTORY=/opt/minecraft
+S3_BUCKET_NAME="s3://bradfordly-things"
+MINECRAFT_SAVE_FILE="/games/saves/minecraft/ce_plays_mc.zip"
+PALWORLD_SAVE_FILE="/games/saves/palworld/ce_plays_pw.zip"
 
 ### install dependencies ###
 dpkg --add-architecture i386
@@ -32,24 +31,29 @@ depList=(
 )
 apt update && apt install ${depList[@]} -y
 
+### linuxgsm setup
+curl -Lo linuxgsm.sh https://linuxgsm.sh
+chmod +x linuxgsm.sh
+
 ### palworld setup
+ufw allow 2811
 adduser pwserver
-su - pwserver
-#TODO: cli call to aws secrets manager
+echo "pwserver:${pwserver}" | chpasswd
+bash linuxgsm.sh pwserver
 
-### minecraft setup ###
-#ufw allow 25565
-#mkdir ${MINECRAFT_DIRECTORY}
-#cd ${MINECRAFT_DIRECTORY}
+### minecraft setup
+ufw allow 25565
+adduser mcserver
+echo "mcserver:${mcserver}" | chpasswd
+bash linuxgsm.sh mcserver
 
+# TODO: locate mc install directory from linux gsm
 #aws s3 cp ${S3_BUCKET_NAME}${MINECRAFT_SAVE_FILE} ${MINECRAFT_SAVE_FILE}
 #unzip ${MINECRAFT_SAVE_FILE}
 #rm ${MINECRAFT_SAVE_FILE}
-
-#wget ${MINECRAFT_BINARY_URL}
-#java -Xmx1024M -Xms1024M -jar server.jar nogui
 #sed 's/false/true/g' eula.txt
 #sed 's/level-name=minecraft/level-name=ce_plays_mc/g' server.properties
 #sed 's/difficulty=easy/difficulty=normal/g' server.properties
 #sed 's/view-distance=10/view-distance=30/g' server.properties
-###
+
+### dynamic dns (update route53 cname record w/ new ec2 public dns)
