47ca780acec583c64482cc5d2216e3059b4f45c3
Bradfordly <brad@bradfordly.com>
2024-02-23 11:46:12 -0500

    userdata cleanup

--- knowhere/knowhere-userdata.sh
+++ knowhere/knowhere-userdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -xe
 
+### install dependencies ###
 depList=(
+    awscli
     curl
     wget
     file
@@ -25,24 +27,33 @@ dpkg --add-architecture i386
 apt update
 apt install ${depList[@]} -y
 
+### stage linuxgsm script ###
 curl -Lo linuxgsm.sh https://linuxgsm.sh
 chmod +x linuxgsm.sh
 
-declare -A serverList
-serverList['pwserver']=8211 #palworld
-serverList['mcserver']=25565 #minecraft
-
+### install game server files ###
+serverList=(
+    pwserver    #Palworld
+    mcserver    #Minecraft
+    vhserver    #Valheim
+)
 for server in "${!serverList[@]}"
 do
-    password=$server@123
-    port=$serverList[$server]
-    ufw allow $port
-    adduser $server --disabled-password
-    echo "$server:$password" | chpasswd
+    
+    adduser --system $server --disabled-password
+    #password= ;)
+    #echo "$server:$password" | chpasswd
     echo "$server  ALL=(ALL:ALL) ALL" >> /etc/sudoers
     chown -R "$server:$server" /home/$server
-    cp linuxgsm.sh /home/$server/linuxgsm.sh
+    cp linuxgsm.sh /home/$server/linuxgsm.sh 
     /bin/su -c "/home/$server/linuxgsm.sh $server" - $server
-    bash /home/$server/$server install
-    #/bin/su -c "/home/$server/$server auto-install" - $server
+    bash /home/$server/$server install 
+    /bin/su -c "/home/$server/$server auto-install" - $server
 done
+
+### dynamic dns
+wget https://raw.githubusercontent.com/Bradfordly/bradfordly-gamehost/${environment}/knowhere/scripts/ddns.sh
+mv ddns.sh /opt/knowhere/scripts/ddns.sh
+chmod +x /opt/knowhere/scripts/ddns.sh
+bash /opt/knowhere/scripts/ddns.sh
+cp /opt/knowhere/scripts/ddns.sh /etc/init.d/ddns.sh
